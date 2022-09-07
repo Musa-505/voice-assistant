b01527c6ccafe3a89c3a0a6e33f26da225234ab5
univerms <univerms@gmail.com>
2022-09-07 14:00:53 +0600

    update and fixed bugs

--- answer.py
+++ answer.py
@@ -1,2 +1,7 @@
+import random as rm
+an_grad_v1 = 'у меня всё хорошо'
+an_grad_v2 = 'у меня'
+an_grad_v3 = 'у'
+an_grad_list = [an_grad_v1, an_grad_v2, an_grad_v3]
+print(rm.choice(an_grad_list))
 an_cmd = 'пока'
-an_grad = 'у меня всё хорошо'
