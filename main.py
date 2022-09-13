a7bfd36f98112511d43e7917f0b537e433874154
ms_aldanazarov <musaaldanazarov@gmail.com>
2022-09-13 12:00:58 +0600

    update main py and random answer,no add english model

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+from secrets import choice
 from record import listen
 # from speech import play
 from append import audio
+import random
 import command
 import answer
 
 for text in listen():
     if text == command.cmd_exit:
-        audio(speack=answer.an_cmd)
+        audio(speack=random.choice(answer.an_cmd))
         quit()
     elif text == command.cmd_grad:
-        audio(speack=answer.an_grad)
+        audio(speack=random.choice(answer.an_grad))
     else:
         print(text)
